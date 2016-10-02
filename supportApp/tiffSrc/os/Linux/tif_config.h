e3da62b38fc1b5bcaaccf226a4de7eafea42a034
Mark Rivers <rivers@cars.uchicago.edu>
2016-10-02 16:52:00 -0500

    New file

---
+++ supportApp/tiffSrc/os/Linux/tif_config.h
@@ -0,0 +1,6 @@
+#ifdef __LP64_
+  #include tif_config_64.h
+#else
+  #include tif_config_32.h
+#endif
+
---
+++ supportApp/tiffSrc/os/Linux/tiffconf.h
@@ -0,0 +1,6 @@
+#ifdef __LP64_
+  #include tiffconf_64.h
+#else
+  #include tiffconf_32.h
+#endif
+
