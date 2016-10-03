ab1690e29bd97a77696fb0e419f8f73096c99bc1
Mark Rivers <rivers@cars.uchicago.edu>
2016-10-03 17:14:01 -0500

    Added support for mingw

--- supportApp/hdf5Src/os/WIN32/H5Tinit.c
+++ supportApp/hdf5Src/os/WIN32/H5Tinit.c
@@ -1,5 +1,9 @@
-#ifdef _M_X64
-  #include "H5Tinit_win64.c"
+#ifdef __MINGW32__
+  #include "H5Tinit_mingw32.c"
 #else
-  #include "H5Tinit_win32.c"
+  #ifdef _M_X64
+    #include "H5Tinit_win64.c"
+  #else
+    #include "H5Tinit_win32.c"
+  #endif
 #endif
