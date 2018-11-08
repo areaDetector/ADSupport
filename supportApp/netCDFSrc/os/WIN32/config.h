9719e8467584950de3fed89b152c1d809522c999
Mark Rivers <rivers@cars.uchicago.edu>
2018-11-08 17:22:16 -0600

    Generated by running cmake on 64-bit system with Visual Studio 2015 Edited to set SIZEOF_VOIDP etc. on 32 or 64 bit system. Edited to set HAVE_INTTYPES_H on VS2010 or later versions.

--- supportApp/netCDFSrc/os/WIN32/config.h
+++ supportApp/netCDFSrc/os/WIN32/config.h
@@ -1,5 +1,76 @@
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#ifdef _MSC_VER
+
+/* Prevent an issue where there is a circular inclusion
+   of winsock.h/windows.h.  This weird state occurs with
+   libdap4 and hdf4 support. The solution comes from the
+   following URL, found after a bit of research.
+
+   Added in support of the 4.5.0-rc1.  Hello, future generations.
+
+   * https://stackoverflow.com/questions/1372480/c-redefinition-header-files-winsock2-h
+
+   */
+
+/* #define HAVE_WINSOCK2_H
+
+#ifdef HAVE_WINSOCK2_H
+   #define _WINSOCKAPI_
+#endif
+*/
+   #if _MSC_VER>=1900
+     #define STDC99
+   #endif
+/* Define O_BINARY so that the appropriate flags
+are set when opening a binary file on Windows. */
+
+/* Disable a few warnings under Visual Studio, for the
+   time being. */
+   #include <io.h>
+   #pragma warning( disable: 4018 4996 4244 4305 )
+   #define unlink _unlink
+   #define open _open
+   #define close _close
+   #define read _read
+   #define lseek _lseeki64
+
+   #define fstat _fstat64
+
+   #define off_t __int64
+   #define _off_t __int64
+
+   #ifndef _OFF_T_DEFINED
+   #define _OFF_T_DEFINED
+   #endif
+
+   #ifdef _WIN32
+    #ifndef strcasecmp
+      #define strcasecmp _stricmp
+    #endif
+
+    #ifndef snprintf
+     #if _MSC_VER<1900
+       #define snprintf _snprintf
+     #endif
+    #endif
+   #endif
+
+
+   #define strdup _strdup
+   #define fdopen _fdopen
+   #define write _write
+   #define strtoll _strtoi64
+#endif /*_MSC_VER */
+
+/* #undef const */
+
+#ifndef _FILE_OFFSET_BITS
+/* #undef _FILE_OFFSET_BITS */
+/* #undef _LARGEFILE64_SOURCE */
+/* #undef _LARGEFILE_SOURCE */
+#endif
 
 /* Define if building universal (internal helper macro) */
 /* #undef AC_APPLE_UNIVERSAL_BUILD */
@@ -30,11 +101,24 @@
 /* default chunk size in bytes */
 #define DEFAULT_CHUNK_SIZE 4194304
 
-/* if true, enable CDF5 Support */
-#define ENABLE_CDF5 1
+/* set this only when building a DLL under MinGW */
+/* #undef DLL_EXPORT */
 
-/* if true, build DAP Client */
+/* set this only when building a DLL under MinGW */
+/* #undef DLL_NETCDF */
+
+/* if true, enable CDF5 Support */
+/* #undef ENABLE_CDF5 */
+/* #undef USE_CDF5 */
+
+/* if true, enable strict null byte header padding. */
+/* #undef USE_STRICT_NULL_BYTE_HEADER_PADDING */
+
+/* if true, build DAP2 and DAP4 Client */
 /* #undef ENABLE_DAP */
+
+/* if true, build DAP4 Client */
+/* #undef ENABLE_DAP4 */
 
 /* if true, build DAP4 Client */
 /* #undef ENABLE_DAP4 */
@@ -43,32 +127,41 @@
 /* #undef ENABLE_DAP_GROUPS */
 
 /* if true, do remote tests */
-/* #undef ENABLE_DAP_REMOTE_TESTS */
+#define ENABLE_DAP_REMOTE_TESTS 1
 
-/* if true, use _FillValue for NC_ERANGE data elements */
-/* #undef ERANGE_FILL */
+/* define the possible sources for remote test servers */
+#define REMOTETESTSERVERS	"remotetest.unidata.ucar.edu,jetstream.unidata.ucar.edu"
+
+/* if true, run extra tests which may not work yet */
+/* #undef EXTRA_TESTS */
 
 /* use HDF5 1.6 API */
 /* #undef H5_USE_16_API */
 
 /* Define to 1 if you have `alloca', as a function or macro. */
-#define HAVE_ALLOCA 1
+/* #undef HAVE_ALLOCA */
 
 /* Define to 1 if you have <alloca.h> and it should be used (not on Ultrix).
    */
-/* #define HAVE_ALLOCA_H 1 */
+/* #undef HAVE_ALLOCA_H */
+
+/* Define to 1 if you have hdf5_coll_metadata_ops */
+/* #undef HDF5_HAS_COLL_METADATA_OPS */
 
 /* Is CURLINFO_RESPONSE_CODE defined */
-#define HAVE_CURLINFO_RESPONSE_CODE 1
+/* #undef HAVE_CURLINFO_RESPONSE_CODE */
+
+/* Is CURLINFO_HTTP_CODE defined */
+/* #undef HAVE_CURLINFO_HTTP_CONNECTCODE */
 
 /* Is CURLOPT_KEYPASSWD defined */
-#define HAVE_CURLOPT_KEYPASSWD 1
+/* #undef HAVE_CURLOPT_KEYPASSWD */
 
 /* Is CURLOPT_PASSWORD defined */
-#define HAVE_CURLOPT_PASSWORD 1
+/* #undef HAVE_CURLOPT_PASSWORD */
 
 /* Is CURLOPT_USERNAME defined */
-#define HAVE_CURLOPT_USERNAME 1
+/* #undef HAVE_CURLOPT_USERNAME */
 
 /* Define to 1 if you have the declaration of `isfinite', and to 0 if you
    don't. */
@@ -82,47 +175,33 @@
    */
 #define HAVE_DECL_ISNAN 1
 
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#define HAVE_DLFCN_H 1
-
 /* Define to 1 if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
 
-/* Define to 1 if you have the `fsync' function. */
-#define HAVE_FSYNC 1
+/* Define to 1 if you have the BaseTsd.h header file. */
+#define HAVE_BASETSD_H 1
 
-/* Define to 1 if you have the <ftw.h> header file. */
-#define HAVE_FTW_H 1
+/* Define if we have filelengthi64. */
+#define HAVE_FILE_LENGTH_I64 1
+
+
+/* Define to 1 if you have the `fsync' function. */
+/* #undef HAVE_FSYNC */
 
 /* Define to 1 if you have the <getopt.h> header file. */
-#define HAVE_GETOPT_H 1
+/* #undef HAVE_GETOPT_H */
 
 /* Define to 1 if you have the `getpagesize' function. */
-#define HAVE_GETPAGESIZE 1
+/* #undef HAVE_GETPAGESIZE */
 
 /* Define to 1 if you have the `getrlimit' function. */
-/* #define HAVE_GETRLIMIT 1 */
+/* #undef HAVE_GETRLIMIT */
 
 /* Define to 1 if you have the `gettimeofday' function. */
-#define HAVE_GETTIMEOFDAY 1
+/* #undef HAVE_GETTIMEOFDAY */
 
-/* Define to 1 if you have the `H5free_memory' function. */
-/* #undef HAVE_H5FREE_MEMORY */
-
-/* Define to 1 if you have the `H5Pget_fapl_mpio' function. */
-/* #undef HAVE_H5PGET_FAPL_MPIO */
-
-/* Define to 1 if you have the `H5Pget_fapl_mpiposix' function. */
-/* #undef HAVE_H5PGET_FAPL_MPIPOSIX */
-
-/* Define to 1 if you have the `H5Pset_all_coll_metadata_ops' function. */
-/* #undef HAVE_H5PSET_ALL_COLL_METADATA_OPS */
-
-/* Define to 1 if you have the `H5Pset_deflate' function. */
-/* #undef HAVE_H5PSET_DEFLATE */
-
-/* Define to 1 if you have the `H5Pset_libver_bounds' function. */
-/* #undef HAVE_H5PSET_LIBVER_BOUNDS */
+/* if true, netcdf4 file properties will be set using H5Pset_libver_bounds */
+/* #undef HDF5_HAS_LIBVER_BOUNDS */
 
 /* Define to 1 if you have the `H5Z_SZIP' function. */
 /* #undef HAVE_H5Z_SZIP */
@@ -130,23 +209,27 @@
 /* Define to 1 if you have the <hdf5.h> header file. */
 /* #undef HAVE_HDF5_H */
 
+/* Define to 1 if you have the <hdf5.h> header file. */
+/* #undef HAVE_HDF5_HL_H */
+
 /* Define to 1 if the system has the type `int64'. */
 /* #undef HAVE_INT64 */
 
 /* Define to 1 if you have the <inttypes.h> header file. */
+#if _MSC_VER < 1700
 /* #define HAVE_INTTYPES_H 1 */
+#else
+  #define HAVE_INTTYPES_H 1
+#endif
 
-/* Define to 1 if you have the `df' library (-ldf). */
-/* #undef HAVE_LIBDF */
-
-/* Define to 1 if you have the <libgen.h> header file. */
-#define HAVE_LIBGEN_H 1
+/* Define to 1 if you have the `dl' library (-ldl). */
+/* #undef HAVE_LIBDL */
 
 /* Define to 1 if you have the `jpeg' library (-ljpeg). */
 /* #undef HAVE_LIBJPEG */
 
 /* Define to 1 if you have the `m' library (-lm). */
-#define HAVE_LIBM 1
+/* #undef HAVE_LIBM */
 
 /* Define to 1 if you have the `mfhdf' library (-lmfhdf). */
 /* #undef HAVE_LIBMFHDF */
@@ -160,8 +243,8 @@
 /* Define to 1 if the system has the type `longlong'. */
 /* #undef HAVE_LONGLONG */
 
-/* Define to 1 if the system has the type `long long int'. */
-/* #undef HAVE_LONG_LONG_INT */
+/* Define to 1 if the system has the type 'long long int'. */
+#define HAVE_LONG_LONG_INT 1
 
 /* Define to 1 if you have the <malloc.h> header file. */
 #define HAVE_MALLOC_H 1
@@ -169,14 +252,8 @@
 /* Define to 1 if you have the `memmove' function. */
 #define HAVE_MEMMOVE 1
 
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
-/* Define to 1 if you have the <mfhdf.h> header file. */
-/* #undef HAVE_MFHDF_H */
-
 /* Define to 1 if you have the `mkstemp' function. */
-/* #define HAVE_MKSTEMP 1 */
+/* #undef HAVE_MKSTEMP */
 
 /* Define to 1 if you have the `mktemp' function. */
 #define HAVE_MKTEMP 1
@@ -185,22 +262,16 @@
 /* #undef HAVE_MPI_COMM_F2C */
 
 /* Define to 1 if you have the `mremap' function. */
-#define HAVE_MREMAP 1
+/* #undef HAVE_MREMAP */
 
 /* Define to 1 if you have the `random' function. */
-/* #define HAVE_RANDOM 1 */
-
-/* Define to 1 if the system has the type `schar'. */
-/* #undef HAVE_SCHAR */
-
-/* Define to 1 if the system has the type `size_t'. */
-#define HAVE_SIZE_T 1
+/* #undef HAVE_RANDOM */
 
 /* Define to 1 if you have the `snprintf' function. */
 #define HAVE_SNPRINTF 1
 
 /* Define to 1 if the system has the type `ssize_t'. */
-/* #define HAVE_SSIZE_T 1 */
+/* #undef HAVE_SSIZE_T */
 
 /* Define to 1 if you have the <stdarg.h> header file. */
 #define HAVE_STDARG_H 1
@@ -217,11 +288,23 @@
 /* Define to 1 if you have the `strdup' function. */
 #define HAVE_STRDUP 1
 
+/* Define to 1 if you have the `strndup` function. */
+/* #undef HAVE_STRNDUP */
+
 /* Define to 1 if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
+/* #undef HAVE_STRINGS_H */
+
+/* Define to 1 if you have the <signal.h> header file. */
+#define HAVE_SIGNAL_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <ftw.h> header file. */
+/* #undef HAVE_FTW_H */
+
+/* Define to 1 if you have the <libgen.h> header file. */
+/* #undef HAVE_LIBGEN_H */
 
 /* Define to 1 if you have the `strlcat' function. */
 /* #undef HAVE_STRLCAT */
@@ -232,27 +315,26 @@
 /* Define to 1 if you have the `strtoull' function. */
 #define HAVE_STRTOULL 1
 
-/* Define to 1 if `st_blksize' is a member of `struct stat'. */
-/* #define HAVE_STRUCT_STAT_ST_BLKSIZE 1 */
+/* Define to 1 if you have the `stroull' function. */
+/* #undef HAVE_STROULL */
 
-/* Define to 1 if your `struct stat' has `st_blksize'. Deprecated, use
-   `HAVE_STRUCT_STAT_ST_BLKSIZE' instead. */
-#define HAVE_ST_BLKSIZE 1
+/* Define to 1 if `st_blksize' is a member of `struct stat'. */
+/* #undef HAVE_STRUCT_STAT_ST_BLKSIZE */
 
 /* Define to 1 if you have the `sysconf' function. */
-#define HAVE_SYSCONF 1
+/* #undef HAVE_SYSCONF */
 
 /* Define to 1 if you have the <sys/param.h> header file. */
-/* #define HAVE_SYS_PARAM_H 1 */
+/* #undef HAVE_SYS_PARAM_H */
 
 /* Define to 1 if you have the <sys/resource.h> header file. */
-/* #define HAVE_SYS_RESOURCE_H 1 */
+/* #undef HAVE_SYS_RESOURCE_H */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/time.h> header file. */
-#define HAVE_SYS_TIME_H 1
+/* #undef HAVE_SYS_TIME_H */
 
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
@@ -261,29 +343,21 @@
 /* #undef HAVE_UCHAR */
 
 /* Define to 1 if the system has the type `uint'. */
-/* #define HAVE_UINT 1 */
+/* #undef HAVE_UINT */
 
 /* Define to 1 if the system has the type `uint64'. */
 /* #undef HAVE_UINT64 */
 
 /* Define to 1 if you have the <unistd.h> header file. */
-/* #define HAVE_UNISTD_H 1 */
-
-/* Define to 1 if the system has the type `unsigned long long int'. */
-/* #undef HAVE_UNSIGNED_LONG_LONG_INT */
+/* #undef HAVE_UNISTD_H */
+#define YY_NO_UNISTD_H 1
 
 /* Define to 1 if the system has the type `ushort'. */
-/* #define HAVE_USHORT 1 */
-
-/* if true, use collective metadata ops in parallel netCDF-4 */
-/* #undef HDF5_HAS_COLL_METADATA_OPS */
+/* #undef HAVE_USHORT */
 
 /* if true, H5free_memory() will be used to free hdf5-allocated memory in
    nc4file. */
 /* #undef HDF5_HAS_H5FREE */
-
-/* if true, netcdf4 file properties will be set using H5Pset_libver_bounds */
-/* #undef HDF5_HAS_LIBVER_BOUNDS */
 
 /* if true, hdf5 has parallelism enabled */
 /* #undef HDF5_PARALLEL */
@@ -297,9 +371,6 @@
 /* If true, turn on logging. */
 /* #undef LOGGING */
 
-/* Define to the sub-directory where libtool stores uninstalled libraries. */
-#define LT_OBJDIR ".libs/"
-
 /* max size of the default per-var chunk cache. */
 #define MAX_DEFAULT_CACHE_SIZE 67108864
 
@@ -308,6 +379,18 @@
 
 /* no IEEE float on this platform */
 /* #undef NO_IEEE_FLOAT */
+
+#define BUILD_V2 1
+/* #undef ENABLE_DOXYGEN */
+/* #undef ENABLE_INTERNAL_DOCS */
+/* #undef VALGRIND_TESTS */
+/* #undef ENABLE_CDMREMOTE */
+/* #undef USE_HDF5 */
+/* #undef ENABLE_FILEINFO */
+/* #undef TEST_PARALLEL */
+/* #undef BUILD_RPC */
+#define USE_X_GETOPT 1
+#define ENABLE_EXTREME_NUMBERS 1
 
 /* do not build the netCDF version 2 API */
 /* #undef NO_NETCDF_2 */
@@ -333,11 +416,30 @@
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "4.6.1"
 
-/* if true, NC_EINVALCOORDS check is more relaxed */
-/* #undef RELAX_COORD_BOUND */
+/* The size of `ulonglong` as computed by sizeof. */
+#define SIZEOF_ULONGLONG 8
 
-/* the testservers for remote tests. */
-#define REMOTETESTSERVERS "remotetest.unidata.ucar.edu,jetstream.unidata.ucar.edu"
+/* The size of `longlong` as computed by sizeof. */
+#define SIZEOF_LONGLONG 8
+
+/* The size of `char` as computed by sizeof. */
+#define SIZEOF_CHAR 1
+
+/* The size of `uchar` as computed by sizeof. */
+#define SIZEOF_UCHAR 1
+
+/* The size of `__int64` found on Windows systems. */
+/* #undef SIZEOF___INT64 */
+
+/* The size of `void*` as computed by sizeof. */
+#ifdef _M_X64
+  #define SIZEOF_VOIDSTAR 8
+#else
+  #define SIZEOF_VOIDSTAR 4
+#endif
+
+/* The size of `short` as computed by sizeof. */
+/* #undef SIZEOF_OFF64_T */
 
 /* The size of `double', as computed by sizeof. */
 #define SIZEOF_DOUBLE 8
@@ -367,55 +469,30 @@
   #define SIZEOF_SIZE_T 4
 #endif
 
-/* The size of `ssize_t', as computed by sizeof. */
-#define SIZEOF_SSIZE_T 8
-
-/* The size of `uchar', as computed by sizeof. */
-/* #undef SIZEOF_UCHAR */
-
 /* The size of `uint', as computed by sizeof. */
-#define SIZEOF_UINT 4
-
-/* The size of `unsigned char', as computed by sizeof. */
-#define SIZEOF_UNSIGNED_CHAR 1
+/* #undef SIZEOF_UINT */
 
 /* The size of `unsigned int', as computed by sizeof. */
-/* #undef SIZEOF_UNSIGNED_INT */
+#define SIZEOF_UNSIGNED_INT 4
 
 /* The size of `unsigned long long', as computed by sizeof. */
 #define SIZEOF_UNSIGNED_LONG_LONG 8
 
 /* The size of `unsigned short int', as computed by sizeof. */
-/* #undef SIZEOF_UNSIGNED_SHORT_INT */
+#define SIZEOF_UNSIGNED_SHORT_INT 2
 
 /* The size of `ushort', as computed by sizeof. */
-#define SIZEOF_USHORT 2
+/* #undef SIZEOF_USHORT */
 
 /* The size of `void*', as computed by sizeof. */
-#define SIZEOF_VOIDP 8
 #ifdef _M_X64
   #define SIZEOF_VOIDP 8
 #else
   #define SIZEOF_VOIDP 4
 #endif
 
-
-/* If using the C implementation of alloca, define if you know the
-   direction of stack growth for your system; otherwise it will be
-   automatically deduced at runtime.
-	STACK_DIRECTION > 0 => grows toward higher addresses
-	STACK_DIRECTION < 0 => grows toward lower addresses
-	STACK_DIRECTION = 0 => direction of growth unknown */
-/* #undef STACK_DIRECTION */
-
-/* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
-
 /* Place to put very large netCDF test files. */
 #define TEMP_LARGE "."
-
-/* if true, enable CDF5 Support */
-#define USE_CDF5 1
 
 /* if true, build DAP Client */
 /* #undef USE_DAP */
@@ -466,9 +543,6 @@
 /* if true, use stdio instead of posixio */
 /* #undef USE_STDIO */
 
-/* if true, enable strict null byte header padding */
-/* #undef USE_STRICT_NULL_BYTE_HEADER_PADDING */
-
 /* if true, compile in szip compression in netCDF-4 variables */
 /* #undef USE_SZIP */
 
@@ -483,7 +557,7 @@
 # endif
 #else
 # ifndef WORDS_BIGENDIAN
-/* #  undef WORDS_BIGENDIAN */
+/* #undef WORDS_BIGENDIAN */
 # endif
 #endif
 
@@ -492,31 +566,31 @@
 # define _DARWIN_USE_64_BIT_INODE 1
 #endif
 
-/* Number of bits in a file offset, on hosts where this is settable. */
-/* #undef _FILE_OFFSET_BITS */
-
 /* Define for large files, on AIX-style hosts. */
 /* #undef _LARGE_FILES */
 
-/* Define to empty if `const' does not conform to ANSI C. */
-/* #undef const */
-
 /* Define to `long int' if <sys/types.h> does not define. */
 /* #undef off_t */
-#define off_t __int64
-#define _off_t __int64
-#define _OFF_T_DEFINED
-#include <io.h>
-#include <process.h>
-#define lseek _lseeki64
 
 /* Define to `unsigned int' if <sys/types.h> does not define. */
 /* #undef size_t */
 
+/* Define to `unsigned long if <sys/types.h> does not define. */
+/* #undef uintptr_t */
+
 /* Define strcasecmp, snprintf on Win32 systems. */
 #ifdef _WIN32
-	#define strcasecmp _stricmp
-/*	#define snprintf _snprintf */
+    #ifndef HAVE_STRCASECMP
+	 #define strcasecmp _stricmp
+    #endif
+
+    #ifndef HAVE_SNPRINTF
+	 #define snprintf _snprintf
+    #endif
 #endif
 
+/* #undef WORDS_BIGENDIAN */
+
 #include "ncconfigure.h"
+
+#endif
