9d3f99e0d2ab790fd892f4bca23accb2b079d577
Xiaoqiang Wang <xiaoqiangwang AT gmail DOT com>
2018-12-19 14:41:50 +0100

    build bitshuffle filter inside hdf5

--- supportApp/hdf5Src/H5Z.c
+++ supportApp/hdf5Src/H5Z.c
@@ -34,6 +34,10 @@
 #   include "blosc_filter.h"
 #endif
 
+#ifdef H5_HAVE_FILTER_BSHUF
+#   include "bshuf_h5filter.h"
+#endif
+
 /* Local typedefs */
 #ifdef H5Z_DEBUG
 typedef struct H5Z_stats_t {
@@ -117,6 +121,10 @@ H5Z__init_package(void)
     if(H5Z_register(BLOSC_FILTER) < 0)
         HGOTO_ERROR (H5E_PLINE, H5E_CANTINIT, FAIL, "unable to register blosc filter")
 #endif /* H5_HAVE_FILTER_BLOSC */
+#ifdef H5_HAVE_FILTER_BSHUF
+    if(bshuf_register_h5filter() < 0)
+        HGOTO_ERROR (H5E_PLINE, H5E_CANTINIT, FAIL, "unable to register bshuf filter")
+#endif /* H5_HAVE_FILTER_BSHUF */
 
 done:
     FUNC_LEAVE_NOAPI(ret_value)
